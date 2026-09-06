d1bb077f40724bf71998ac1a3a9f5583c4c975e7
ChatandChill <hello.ChatandChill@gmail.com>
2026-09-06 15:16:12 +0100

    Add LiveGiftSystemPro component

---
+++ app/components/livegiftsystempro.js
@@ -0,0 +1,4 @@
+"use client";
+export default function LiveGiftSystemPro() {
+  return <div style={{padding:20, border:'1px solid #333', borderRadius:12, margin:10}}>🎁 Live Gift System Pro v6.7.0</div>;
+}
