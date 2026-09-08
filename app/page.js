b694e2fbe6b37fa789f3e1dfa8d8bdb89e18df50
ChatandChill <hello.ChatandChill@gmail.com>
2026-09-08 05:27:24 +0100

    Refactor Home component to include FundWalletSystemPro
    Removed unused components and added FundWalletSystemPro.

--- app/page.js
+++ app/page.js
@@ -1,14 +1,11 @@
-import AppLogoSystem from './components/AppLogoSystem';
-import WorldNo1System from './components/WorldNo1System';
-import BadgeSystemPro from './components/BadgeSystemPro';
+import FundWalletSystemPro from './components/FundWalletSystemPro'
 
 export default function Home() {
   return (
-    <main style={{background:'#0a0a0a', color:'white', minHeight:'100vh', padding:'20px'}}>
-      <AppLogoSystem />
-      <WorldNo1System />
-      <BadgeSystemPro />
-      <h1 style={{textAlign:'center', marginTop:'50px'}}>Site is back - testing fund</h1>
+    <main>
+      {/* ... your other components */}
+      <FundWalletSystemPro />
+      <h1>Site is back - testing fund</h1>
     </main>
-  );
+  )
 }
